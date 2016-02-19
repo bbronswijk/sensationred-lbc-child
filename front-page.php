d2a90831633020f714fa9b8dfa7ad25423419e1e
bbronswijk <brambronswijk@gmail.com>
2016-02-19 23:37:35 +0100

    fix repsonsive styling

--- front-page.php
+++ front-page.php
@@ -6,8 +6,9 @@
 	<?php get_header(); ?>
 	<div class="main_home container">	
 		<div class="wrapper">	
-			<div class="homepage_block" style="float:left; width:435px;">
-				<iframe width="435" height="245" src="//www.youtube.com/embed/gpvZcJSPUEU?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+			
+			<div class="homepage_column">
+				<iframe width="440" height="245" src="//www.youtube.com/embed/gpvZcJSPUEU?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
 			
 				<!-- If there is an active sidebar show it --> 	
 				<div class="shortcut_menu homepage_block">
@@ -28,34 +29,33 @@
 						}
 					}
 					?>
-					<div class="bg_sc_menu">background</div>
 				</div>
 			</div>
 			
-			<div class="shortcut_menu homepage_block">
-				<div class="title">
-					<h1>Evenementen</h1>	
+			<div class="homepage_column">
+				<div class="shortcut_menu homepage_block">
+					<div class="title">
+						<h1>Evenementen</h1>	
+					</div>
+					<?php wp_reset_query();						
+						$args = array(
+							'post_type' => 'evenementen',
+							'posts_per_page' => 100
+						);
+						$query = new WP_Query( $args );
+						
+						if ( $query->have_posts() ){ 
+							while ( $query->have_posts() ) { $query->the_post();
+								//global $more;
+								//$more = 0;	
+								?>
+								<h2><?php the_title(); ?></h2>
+								<?php								
+							} // end while
+						} // end if
+					?>
 				</div>
-				<?php wp_reset_query();						
-					$args = array(
-						'post_type' => 'evenementen',
-						'posts_per_page' => 100
-					);
-					$query = new WP_Query( $args );
-					
-					if ( $query->have_posts() ){ 
-						while ( $query->have_posts() ) { $query->the_post();
-							//global $more;
-							//$more = 0;	
-							?>
-							<h2><?php the_title(); ?></h2>
-							<?php								
-						} // end while
-					} // end if
-				?>
-				<div class="bg_sc_menu">background</div>
 			</div>
-	
 		</div> <!-- wrapper -->
 	</div><!-- container --> 
 	
